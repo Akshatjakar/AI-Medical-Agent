f7866d31766318064a65f0e9580d7110c401befb
akshat <akshatjakar2@gmail.com>
2026-05-28 19:37:01 +0530

    fixed clerk error

--- app/(routes)/dashboard/_components/AppHeader.tsx
+++ app/(routes)/dashboard/_components/AppHeader.tsx
@@ -50,7 +50,7 @@ function AppHeader() {
         </nav>
 
         {/* User Profile */}
-        <UserButton afterSignOutUrl="/" />
+        <UserButton />
       </div>
 
       {/* Extra Animations */}
